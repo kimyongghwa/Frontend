e1fe993b2f375f9b39ad6c3c40812f43f74b80d9
JaeHyun Kim <carbitna03@gmail.com>
2020-09-29 11:13:26 +0900

    update

---
+++ PositionMove/index.js
@@ -0,0 +1,10 @@
+const crow = document.getElementsByClassName('crowimg')[0]
+const button = document.getElementsByClassName('btn')[0]
+var h = 33
+
+function fly(){
+    h-=5
+    crow.style.top = h+"vh"
+}
+
+
